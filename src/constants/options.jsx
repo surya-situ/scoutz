afc0a74071f903a9876e2cce981860902aa7543e
surya-situ <suryasocial55@gmail.com>
2025-01-10 17:17:22 +0530

    doc: meta data for option selection and prompt for gemini ai

---
+++ src/constants/options.jsx
@@ -0,0 +1,53 @@
+export const SelectTravelsLists = [
+  {
+    id: 1,
+    title: "Solo",
+    desc: "Explore the world solo at your own pace.",
+    icon: "🤠",
+    people: "1",
+  },
+  {
+    id: 2,
+    title: "Couple",
+    desc: "Enjoy the journey together, hand in hand.",
+    icon: "🥂",
+    people: "2 people",
+  },
+  {
+    id: 3,
+    title: "Family",
+    desc: "A family vacation filled with fun and adventure.",
+    icon: "🏡",
+    people: "3 to 5 people",
+  },
+  {
+    id: 4,
+    title: "Group",
+    desc: "Unite with friends for a thrilling adventure.",
+    icon: "🚤",
+    people: "5 to 10 people",
+  },
+];
+
+export const SelectBudgetOptions = [
+  {
+    id: 1,
+    title: "Basic",
+    desc: "Enjoy your trip without breaking the bank.",
+    icon: "💲",
+  },
+  {
+    id: 2,
+    title: "Standard",
+    desc: "Enjoy a comfortable experience without overspending.",
+    icon: "🪙",
+  },
+  {
+    id: 3,
+    title: "Luxury",
+    desc: "The ultimate in comfort and premium experiences.",
+    icon: "💎",
+  },
+];
+
+export const AI_PROMPT ="Generate Travel Plan for Location: {location}, for {totalDays} Days for a {people} with a {budget} budget, give me a Hotels options list with Hotel name, Hotel address, Price, hotel images, geo coordinates, rating, descriptions and suggest itinerary with place name, Place Details, place images, Geo Coordinates, ticket Pricing, Time travel for each of the locations for {totalDays} days with each day plan and the best time to visit, in JSON format"
